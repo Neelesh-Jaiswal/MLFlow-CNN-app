82a2eed9143c007949762abdfb0619beeed96d43
Neelesh-Jaiswal <nikhilneelesh1@gmail.com>
2022-03-07 15:38:10 +0530

    automating the creation of log directories

--- src/main.py
+++ src/main.py
@@ -5,23 +5,27 @@ import os
 import logging
 from src.utils.common import read_yaml, create_directories
 
+STAGE = "MAIN"  ## <<< change stage name
 
-STAGE = "MAIN" ## <<< change stage name
+# automating the log directory creation
+create_directories(['logs'])
+with open(os.path.join('logs', 'running_logs.log'), 'w') as f:
+    f.write('')
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'),
     level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a"
-    )
+)
 
 
 def main():
     with mlflow.start_run() as run:
         mlflow.run(".", "get_data", use_conda=False)
-        # mlflow.run(".", "get_data", parameters={}, use_conda=False) # to show that we can pass parameters (if the function need extra parameters)
+        # mlflow.run(".", "get_data", parameters={}, use_conda=False) # to show that we can pass parameters (if the
+        # function need extra parameters)
         mlflow.run(".", "base_model_creation", use_conda=False)
-
 
 
 if __name__ == '__main__':
