116b404211bbd1eb432e2a8abbd669eda178ca9a
Neelesh-Jaiswal <nikhilneelesh1@gmail.com>
2022-03-07 11:23:38 +0530

    adding conditions on when to unzip

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -38,9 +38,12 @@ def main(config_path):
 
     # unzip operation
     unzip_data_dir = config['data']['unzip_data_dir']
-    create_directories([unzip_data_dir])
-    unzip_file(source=data_file_path, dest=unzip_data_dir)
 
+    if not os.path.exists(unzip_data_dir):
+        create_directories([unzip_data_dir])
+        unzip_file(source=data_file_path, dest=unzip_data_dir)
+    else:
+        logging.info(f'Data already extracted')
     # validating data
     validate_image(config)
 
